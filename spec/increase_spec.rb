2472969a2f6d8c5feac82ea7377ff07ace3272cb
Gary Tou <gary@garytou.com>
2023-03-14 18:27:22 -0700

    Remove unless test

--- spec/increase_spec.rb
+++ spec/increase_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe Increase do
     expect(Increase::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
+  # it "does something useful" do
+  #   expect(false).to eq(true)
+  # end
 end
