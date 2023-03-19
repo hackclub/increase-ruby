8bde5e42fe26fb4dd2d47cef467e2ea966f9e565
Gary Tou <gary@garytou.com>
2023-03-19 03:56:54 -0700

    `rake standard:fix`

--- lib/increase/resources.rb
+++ lib/increase/resources.rb
@@ -1,4 +1,4 @@
 # Require all files in the lib/increase/resources directory
-Dir[File.expand_path("resources/*.rb", __dir__)].each do |file|
+Dir[File.expand_path("resources/*.rb", __dir__)].sort.each do |file|
   require file
 end
